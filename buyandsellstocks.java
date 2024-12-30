1c38255ca6716233035cb9a45aa57c675ed09af2
Disha Rathore <139526277+appledog632@users.noreply.github.com>
2024-12-31 01:51:43 +0530

    updated

--- buyandsellstocks.java
+++ buyandsellstocks.java
@@ -1,21 +1,15 @@
 class BuyandSellstocks {
-    public static int maxProfit(int[] prices) {
-        int buyprice =Integer.MAX_VALUE;
-        int maxprofit=0;
-
-        for(int i=0;i<prices.length;i++)
-        {
-            if(buyprice<prices[i])
-            {
-                int profit = prices[i] - buyprice;
-                maxprofit = Math.max(maxprofit,profit);
-            }
-            else
-            {
-                buyprice = prices[i];
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else if (prices[i] - buy > profit) {
+                profit = prices[i] - buy;
             }
         }
-        return maxprofit; 
+        return profit;
     }
     public static void main(String args[])
     {
