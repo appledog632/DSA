48a3527186a317fc6a63c84262680743fe9dbc04
Disha Rathore <139526277+appledog632@users.noreply.github.com>
2025-01-16 23:16:55 +0530

    optimised

--- duplicates.java
+++ duplicates.java
@@ -1,11 +1,18 @@
-public class duplicates {
+class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=1;i< nums.length ; i++)
-        {
-            if( nums[i] == nums[i-1])
-            {
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
                 return true;
+            }
+            if (nums[i] < nums[i - 1]) {
+                int t = nums[i];
+                for (int j = i - 2; j >= 0; j--) {
+                    if (t == nums[j]) {
+                        return true;
+                    }
+                }
+                nums[i] = nums[i - 1];
+                nums[i - 1] = t;
             }
         }
         return false;
