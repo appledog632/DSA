b0ac4cbecaf2ade76af452fd234ea7af6bf89ba8
Disha Rathore <139526277+appledog632@users.noreply.github.com>
2025-01-19 20:36:16 +0530

    Create searchRoatedArray.java

---
+++ searchRoatedArray.java
@@ -0,0 +1,27 @@
+public class searchRoatedArray {
+    public int search(int[] arr, int key) {
+        int s= 0;
+        int e= arr.length-1;
+
+        while(s <= e) {
+            int mid= s+ (e-s)/2;
+
+            if(arr[mid]==key)
+                return mid;
+            
+            if(arr[s]<=arr[mid]) {
+                if(arr[s]<=key && key<=arr[mid])
+                    e= mid-1;
+                else
+                    s= mid+1;
+            } else {
+                if(arr[mid]<=key && key<=arr[e])
+                    s= mid+1;
+                else
+                    e= mid-1;
+            }
+
+        }
+        return -1;
+    }
+}
