e45f0fd070a99e5b58dbae2304ae0da0c7203250
Disha Rathore <139526277+appledog632@users.noreply.github.com>
2025-02-09 14:11:16 +0530

    updated

--- TwoPairsum.java
+++ TwoPairsum.java
@@ -1,12 +1,22 @@
-//sorted array is must
+//sorted rotated array
 import java.util.*;
-public class Onepairsum
+public class TwoPairSum
 {
       public static Boolean summ(ArrayList<Integer> list ,int target)
       {
-            int lp=0;
-            int rp =list.size()-1;
-            while(lp<rp)
+            int bp = -1;
+            int n= list.size();
+            for(int i = 0;i< n ;i ++)
+            {
+                  if(list.get(i)> list.get(i+1))
+                  {
+                        bp=i;
+                        break;
+                  }
+            }
+            int lp =bp+1;
+            int rp = bp;
+            while(lp!=rp)
             {
                   if(list.get(lp) + list.get(rp) == target)
                   {
@@ -14,22 +24,22 @@ public class Onepairsum
                   }
                   else if(list.get(lp) + list.get(rp) < target)
                   {
-                        lp++;
+                        lp = (lp-1)%n;
                   }
                   else{
-                        rp--;
+                        rp = (n+rp -1)%n;
                   }
             }
             return false;
       }
 	public static void main(String[] args) {
 	      ArrayList<Integer> list = new ArrayList<>();
-	      list.add(1);
-	      list.add(2);
-	      list.add(3);
-	      list.add(4);
-	      list.add(5);
+	      list.add(11);
+	      list.add(15);
 	      list.add(6);
+	      list.add(8);
+	      list.add(9);
+	      list.add(10);
 	      System.out.print(summ(list ,16));
 	}
 }
